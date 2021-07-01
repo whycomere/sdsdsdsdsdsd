0c6df7d1f761ea482881b45f99c37d73c344bcc4
whycomere <alxjfkd123@naver.com>
2021-07-01 10:28:26 +0900

    2021.7.1 5ban class

--- djangoProject3/settings.py
+++ djangoProject3/settings.py
@@ -20,8 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-local_env = open(os.path.join(BASE_DIR, '.env'))
 
+# os는 운영체제 관련 모듈, 경로관련, base dir란?? 프로젝트폴더의 최종경로
+
+local_env = open(os.path.join(BASE_DIR, '.env'))
 env_list = dict()
 
 while True:
